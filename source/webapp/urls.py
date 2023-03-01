b6d8655c9c684fb175fd776172317f07bd8c58f7
Abylay Kuvatov <abukuvatov@icloud.com>
2023-03-02 01:35:31 +0600

    Добавил страницу и классовое представление детального просмотра задачи

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from webapp.views.issues import IssuesView
+from webapp.views.issues import IssuesView, IssueDetailsView, IssuesRedirectView
 
 urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
-    path("", IssuesView.as_view(), name="issues_view")
+    path('', IssuesView.as_view(), name='issues_view'),
+    path('issues', IssuesRedirectView.as_view(), name='issues_redirect_view'),
+    path('issue/<int:pk>', IssueDetailsView.as_view(), name='issue_details_view'),
 ]
--- source/webapp/views/issues.py
+++ source/webapp/views/issues.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, RedirectView
 
 from webapp.models import Issue
 
@@ -8,5 +9,18 @@ class IssuesView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['issues'] = Issue.objects.all()
+        context['issues'] = Issue.objects.all().order_by('pk')
         return context
+
+
+class IssueDetailsView(TemplateView):
+    template_name = 'issue_details_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issue'] = get_object_or_404(Issue, pk=kwargs['pk'])
+        return context
+
+
+class IssuesRedirectView(RedirectView):
+    pattern_name = 'issues_view'
