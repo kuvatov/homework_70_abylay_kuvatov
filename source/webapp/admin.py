71804176046528589c1ab81b888b007842a20c02
Abylay Kuvatov <abukuvatov@icloud.com>
2023-03-01 23:59:35 +0600

    Добавил модели типов и статусов задач, а также модель самих задач. Сделал миграцию, зарегистрировал модели в админке. Добавил данные в БД и создал фикстуру

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -8,7 +8,7 @@ class IssueAdmin(admin.ModelAdmin):
     list_display = ['summary', 'description', 'status', 'type', 'created_at']
     list_filter = ['summary', 'status', 'type', 'created_at']
     search_fields = ['summary', 'status', 'type', 'created_at']
-    fields = ['summary', 'status', 'type', 'created_at', 'edited_at']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'edited_at']
     readonly_fields = ['created_at', 'edited_at']
 
 
