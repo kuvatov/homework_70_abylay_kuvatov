548cf2ca5e19dc577b9351c28529641d56c5a4a3
Abylay Kuvatov <abukuvatov@icloud.com>
2023-03-02 18:04:22 +0600

    Применил окончательные стили, а также добавил js скрипт для автоматического расширения окна textarea. Завершил этапы 1 и 2 ДЗ #57

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,9 +1,13 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
-from webapp.models import Issue
+from webapp.models import Issue, IssueType, IssueStatus
 
 
 class IssueForm(forms.ModelForm):
+    type = forms.ModelChoiceField(queryset=IssueType.objects.all())
+    status = forms.ModelChoiceField(queryset=IssueStatus.objects.all())
+
     class Meta:
         model = Issue
         fields = ('summary', 'description', 'type', 'status')
@@ -13,3 +17,9 @@ class IssueForm(forms.ModelForm):
             'type': 'Тип',
             'status': 'Статус'
         }
+
+    def clean_summary(self):
+        summary = self.cleaned_data.get('summary')
+        if len(summary) < 5:
+            raise ValidationError('Краткое описание должно состоять минимум из 5 символов!')
+        return summary.capitalize()
