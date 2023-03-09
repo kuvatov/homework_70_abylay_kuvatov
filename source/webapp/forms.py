7682112c93fafc1a16b0b67d35173369ad527bc6
Abylay Kuvatov <abukuvatov@icloud.com>
2023-03-09 19:15:24 +0600

    Добавил поиск по задачам: по вхождению текста в краткое или полное описание

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -23,3 +23,7 @@ class IssueForm(forms.ModelForm):
             'type': 'Тип',
             'status': 'Статус'
         }
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Найти")
--- source/webapp/views/issues.py
+++ source/webapp/views/issues.py
@@ -1,20 +1,50 @@
 from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.http import urlencode
 from django.views import View
 from django.views.generic import TemplateView, RedirectView, ListView
 
-from webapp.forms import IssueForm
+from webapp.forms import IssueForm, SearchForm
 from webapp.models import Issue
 
 
 class IssuesView(ListView):
+    model = Issue
     template_name = 'issues_view.html'
     context_object_name = 'issues'
+    ordering = ['-created_at']
     paginate_by = 10
     paginate_orphans = 1
 
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
     def get_queryset(self):
-        return Issue.objects.all().order_by('-created_at')
+        queryset = super().get_queryset()
+
+        if self.search_value:
+            query = Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class IssueDetailsView(TemplateView):
