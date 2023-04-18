7f6ca053aa4a4aa6409a98e0aaba646fa39c5e9a
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-18 12:02:40 +0600

    Добавил представление редактирования задачи

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from api.views.issues import IssueDetailView
+from api.views.issues import IssueDetailView, IssueUpdateView
 from api.views.projects import ProjectDetailView, ProjectUpdateView, ProjectDeleteView
 
 urlpatterns = [
     path('project/<int:pk>', ProjectDetailView.as_view(), name='project_detail_api'),
     path('project/<int:pk>/update', ProjectUpdateView.as_view(), name='project_update_api'),
     path('project/<int:pk>/delete', ProjectDeleteView.as_view(), name='project_delete_api'),
-    path('issue/<int:pk>', IssueDetailView.as_view(), name='issue_detail_api')
+    path('issue/<int:pk>', IssueDetailView.as_view(), name='issue_detail_api'),
+    path('issue/<int:pk>', IssueUpdateView.as_view(), name='issue_update_api')
 ]
--- source/api/views/issues.py
+++ source/api/views/issues.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,3 +11,13 @@ class IssueDetailView(APIView):
         issue = Issue.objects.get(pk=pk)
         serializer = IssueSerializer(issue)
         return Response(serializer.data)
+
+
+class IssueUpdateView(APIView):
+    def put(self, request, pk):
+        issue = Issue.objects.get(pk=pk)
+        serializer = IssueSerializer(issue, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
